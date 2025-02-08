c6192209505c4c84338e9508aef0c0710cbd6f68
Subhajit-sahu <subhajitsahu133@gmail.com>
2025-02-08 02:03:19 -1200

    Order page added

--- Buy Page/buy.js
+++ Buy Page/buy.js
@@ -1,8 +1,8 @@
-document.getElementById("buy-button").addEventListener("click", function() {
-    let confirmPurchase = confirm("Are you sure you want to buy this machine?");
-    if (confirmPurchase) {
-        alert("🎉 Thank you for your purchase! Your order has been placed. 🚀");
-    } else {
-        alert("No worries! Let us know if you need more details. 😊");
-    }
-});
+// document.getElementById("buy-button").addEventListener("click", function() {
+//     let confirmPurchase = confirm("Are you sure you want to buy this machine?");
+//     if (confirmPurchase) {
+//         alert("🎉 Thank you for your purchase! Your order has been placed. 🚀");
+//     } else {
+//         alert("No worries! Let us know if you need more details. 😊");
+//     }
+// });
